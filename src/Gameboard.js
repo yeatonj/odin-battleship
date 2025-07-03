da5f41caf6596b1de171c49be062f7b2ef027bef
Josh Yeaton <yeaton.josh@gmail.com>
2025-07-02 21:49:24 -0400

    Implement logic for making shots

--- src/Gameboard.js
+++ src/Gameboard.js
@@ -1,20 +1,113 @@
+import Ship from "./Ship";
+
 export default class Gameboard {
     constructor(rows, cols) {
+        if (rows < 4 || cols < 4) {
+            throw new Error("Board too small.");
+        }
         this.rows = rows;
         this.cols = cols;
         this.ships = [];
         this.shots = new Map();
+        this.shipLocs = new Map();
     }
 
     placeShip(row, col, length, orientation) {
-
+        const shipNum = this.ships.length;
+        if (orientation == "right") {
+            // Check OOB
+            if ((col + length - 1) >= this.cols) {
+                throw new Error("Ship out of bounds.");
+            }
+            // Check spots
+            for (let i = col; i < col + length; i++) {
+                const key = this.#keyfromCoord(row, i);
+                if (this.shipLocs[key] != undefined) {
+                    throw new Error("Ship overlap.");
+                }
+            }
+            // No overlap, we can add the ship
+            this.ships.push({
+                "ship": new Ship(length),
+                "row" : row,
+                "col" : col,
+                "orientation" : orientation
+            });
+            for (let i = col; i < col + length; i++) {
+                const key = this.#keyfromCoord(row, i);
+                this.shipLocs[key] = shipNum;
+            }
+        } else if (orientation == "down") {
+            if ((row + length - 1) >= this.rows) {
+                throw new Error("Ship out of bounds.");
+            }
+            // Check spots
+            for (let i = row; i < row + length; i++) {
+                const key = this.#keyfromCoord(i, col);
+                if (this.shipLocs[key] != undefined) {
+                    throw new Error("Ship overlap.");
+                }
+            }
+            // No overlap, we can add the ship
+            this.ships.push({
+                "ship": new Ship(length),
+                "row" : row,
+                "col" : col,
+                "orientation" : orientation
+            });
+            
+            for (let i = row; i < row + length; i++) {
+                const key = this.#keyfromCoord(i, col);
+                this.shipLocs[key] = shipNum;
+            }
+        } else {
+            throw new Error("Improper orientation.");
+        }
+        return true;
     }
 
     recieveAttack(row, col) {
-
+        if (row < 0 || 
+            row >= this.rows ||
+            col < 0 ||
+            col >= this.cols
+        ) {
+            throw new Error("Shot out of bounds.");
+        }
+        // check if already shot here
+        const coord = this.#keyfromCoord(row, col);
+        if (this.shots[coord] != undefined) {
+            throw new Error('Already shot here.');
+        }
+        // Otherwise, check if a ship is here
+        const shipNum = this.shipLocs[coord];
+        if (shipNum === undefined) {
+            this.shots[coord] = 'miss';
+        } else {
+            this.shots[coord] = 'hit';
+            this.ships[shipNum].ship.hit();
+        }
     }
 
     allSunk() {
+        // !! might want to update depending on how we return sunk objects
+        for (const shipStruct of this.ships) {
+            if (!shipStruct.ship.isSunk()) {
+                return false;
+            }
+        }
+        return true;
+    }
 
+    getSunkShips() {
+        // !!
+    }
+
+    getShots() {
+        // !!
+    }
+
+    #keyfromCoord(row, col) {
+        return String(row) + "," + String(col);
     }
 }
--- test/Gameboard.test.js
+++ test/Gameboard.test.js
@@ -4,22 +4,34 @@ import Ship from "../src/Ship.js";
 describe('Gameboard Tests', () => { 
     let gameboard;
 
-    beforeAll(() => {
+    beforeEach(() => {
         gameboard = new Gameboard(5, 5);
+    });
+
+    test('place 1', () => {
+        expect(gameboard.placeShip(1, 1, 3, 'right')).toBe(true);
+    });
+
+    test('place 2', () => {
         gameboard.placeShip(1, 1, 3, 'right');
-        gameboard.placeShip(2, 2, 2, 'down');
+        expect(gameboard.placeShip(2, 2, 2, 'down')).toBe(true);
+    })
+
+    test('bad orientation', () => {
+        expect(() => {gameboard.placeShip(4, 4, 2, 'wrong');}).toThrow();
     });
 
     test('out of bounds ship right', () => {
-        expect(() => gameboard.placeShip(4, 4, 2, 'right')).toThrow();
+        expect(() => {gameboard.placeShip(4, 4, 2, 'right');}).toThrow();
     });
 
     test('out of bounds ship down', () => {
-        expect(() => gameboard.placeShip(4, 4, 2, 'down')).toThrow();
+        expect(() => {gameboard.placeShip(4, 4, 2, 'down');}).toThrow();
     });
 
     test('ship overlap', () => {
-        expect(() => gameboard.placeShip(2, 1, 3, 'right')).toThrow();
+        gameboard.placeShip(2, 2, 2, 'down')
+        expect(() => {gameboard.placeShip(2, 1, 3, 'right');}).toThrow();
     });
 
     test('out of bounds shot 1', () => {
@@ -36,6 +48,39 @@ describe('Gameboard Tests', () => {
 
     test('out of bounds shot 4', () => {
         expect(() => {gameboard.recieveAttack(1, 5)}).toThrow();
+    });
+
+    test('repeat attack', () => {
+        gameboard.recieveAttack(1, 1);
+        expect(() => {gameboard.recieveAttack(1, 1)}).toThrow();
+    });
+
+    test('all sunk one ship', () => {
+        gameboard.placeShip(1, 1, 3, 'right');
+        gameboard.recieveAttack(1, 1);
+        expect(gameboard.allSunk()).toBe(false);
+        gameboard.recieveAttack(1, 2);
+        expect(gameboard.allSunk()).toBe(false);
+        gameboard.recieveAttack(1, 3);
+        expect(gameboard.allSunk()).toBe(true);
+    });
+
+    test('all sunk multiple ships', () => {
+
+        gameboard.placeShip(2, 2, 2, 'down');
+        gameboard.placeShip(1, 1, 3, 'right');
+
+        gameboard.recieveAttack(1, 1);
+        expect(gameboard.allSunk()).toBe(false);
+        gameboard.recieveAttack(1, 2);
+        expect(gameboard.allSunk()).toBe(false);
+        gameboard.recieveAttack(2, 2);
+        expect(gameboard.allSunk()).toBe(false);
+        
+        gameboard.recieveAttack(1, 3);
+        expect(gameboard.allSunk()).toBe(false);
+        gameboard.recieveAttack(3, 2);
+        expect(gameboard.allSunk()).toBe(true);
     });
 
 
