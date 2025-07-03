437151e5f7bdc4df6e2fa82110a90b60282750e0
Josh Yeaton <yeaton.josh@gmail.com>
2025-07-03 13:48:40 -0400

    Add placement logic to controller

--- src/GameController.js
+++ src/GameController.js
@@ -1,5 +1,64 @@
+import Player from "./Player";
+
 export default class GameController {
+    #BOARD_SIZE = 10;
+    #SHIP_SIZES = [5, 4, 3, 3, 2];
+
     constructor() {
-        
+        this.players = [new Player("human", this.#BOARD_SIZE), new Player("computer", this.#BOARD_SIZE)];
+
+        // 0 -> setup, 1 -> playing, 2 -> finished
+        this.gamePhase = 0;
+
+        this.selectedShip = undefined;
+        // 0 -> right, 1 -> down
+        this.selectedOrientation = 0;
+
+        this.placementTracker = []
+        // Place cpu ships randomly and populate placement tracker
+        for (const size of this.#SHIP_SIZES) {
+            this.players[1].randomlyPlaceShip(size);
+            this.placementTracker.push({"length" : size, "placed" : false});
+        }
+    }
+
+    selectShip(shipNum) {
+        this.selectedShip = shipNum;
+    }
+
+    rotateSelectedShip() {
+        this.selectedOrientation = (this.selectedOrientation + 1) % 2;
+    }
+
+    placeSelectedShip(row, col) {
+        if (this.selectedShip == -1) {
+            throw new Error('No ship selected to place.');
+        }
+        if (this.placementTracker[this.selectedShip].placed) {
+            throw new Error('Unable to place already placed ship.');
+        }
+        // Place ship based on current placement
+        const length = this.placementTracker[this.selectedShip].length;
+        const orientation = this.selectedOrientation == 0 ? "right" : "down";
+        try {
+            this.players[0].board.placeShip(row, col, length, orientation);
+            this.placementTracker[this.selectedShip].placed = true;
+            // Reset selected ship
+            this.selectedShip = -1;
+            this.selectedOrientation = 0;
+        } catch {
+            // Could log error placing ship here if we want
+        }
+        if (this.#allPlaced()) {
+            this.#startGame();
+        }
+    }
+
+    #allPlaced() {
+        return this.placementTracker.reduce((accumulator, current) => {return accumulator && current.placed}, true);
+    }
+
+    #startGame() {
+        this.gamePhase = 1;
     }
 }
---
+++ test/GameController.test.js
@@ -0,0 +1,86 @@
+import GameController from "../src/GameController";
+
+describe('Game Controller Tests', () => { 
+    let controller;
+    beforeEach(() => {
+        controller = new GameController();
+    });
+
+    test('player has not placed any ships', () => {
+        expect(controller.gamePhase).toBe(0);
+    });
+
+    test('player tries to place without ship selected 1', () => {
+        expect(() => controller.placeSelectedShip(0, 0)).toThrow();
+    });
+
+    test('player has not placed all ships', () => {
+        controller.selectShip(1);
+        controller.placeSelectedShip(1, 1);
+
+        expect(controller.gamePhase).toBe(0);
+    });
+
+    test('player tries to place without ship selected 2', () => {
+        controller.selectShip(1);
+        controller.placeSelectedShip(1, 1);
+        expect(() => controller.placeSelectedShip(0, 0)).toThrow();
+    });
+
+    test('player has not placed all ships, misplaced one', () => {
+        for (let i = 0; i < 4; i++) {
+            controller.selectShip(i);
+            controller.placeSelectedShip(i,0);
+        }
+        controller.selectShip(4);
+        controller.placeSelectedShip(3,0);
+        
+        expect(controller.gamePhase).toBe(0);
+    });
+
+    test('player has not placed all ships, failed rotation on one', () => {
+        controller.selectShip(0);
+        controller.placeSelectedShip(1,0);
+        controller.selectShip(1);
+        controller.rotateSelectedShip();
+        controller.placeSelectedShip(0,0);
+        controller.selectShip(2);
+        controller.placeSelectedShip(2,0);
+        controller.selectShip(3);
+        controller.placeSelectedShip(3,0);
+        controller.selectShip(4);
+        controller.placeSelectedShip(4,0);
+        expect(controller.gamePhase).toBe(0);
+    });
+
+    test('player tries to place already placed ship', () => {
+        controller.selectShip(1);
+        controller.placeSelectedShip(1, 1);
+        controller.selectShip(1);
+        expect(() => {controller.placeSelectedShip(2, 2)}).toThrow();
+    });
+
+    test('player places all ships 1', () => {
+        for (let i = 0; i < 5; i++) {
+            controller.selectShip(i);
+            controller.placeSelectedShip(i,0);
+        }
+        expect(controller.gamePhase).toBe(1);
+    });
+
+    test('player places all ships 2', () => {
+        controller.selectShip(0);
+        controller.placeSelectedShip(0,0);
+
+        for (let i = 1; i < 5; i++) {
+            controller.selectShip(i);
+            controller.rotateSelectedShip();
+            controller.placeSelectedShip(1,i);
+        }
+        expect(controller.gamePhase).toBe(1);
+        
+    });
+
+    
+
+});
